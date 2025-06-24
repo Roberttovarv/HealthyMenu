84594b3a3ea4cf253ed5cf80924f4681ed05c990
Robert <Roberttovarv@gmail.com>
2025-06-24 15:31:22 +0200

    Fixed

--- screens/CategoriesScreens.js
+++ screens/CategoriesScreens.js
@@ -1,4 +1,4 @@
-import { FlatList } from "react-native-web"
+import { FlatList } from "react-native"
 import { CATEGORIES } from "../data/dummy-data"
 import { CategoryGridTile } from "../components/CategoryGridTile"
 
