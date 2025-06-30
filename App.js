ac7a4c206e6ca242b4c73d7bb0680cbcea2a51fe
Robert <Roberttovarv@gmail.com>
2025-06-30 16:26:52 +0200

    Mientras tanto

--- App.js
+++ App.js
@@ -1,18 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
-import { CategoriesScreen } from './screens/CategoriesScreens';
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
+
+import { CategoriesScreen } from "./screens/CategoriesScreens";
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
-    <SafeAreaView  style={styles.container}>
-    <CategoriesScreen />
+    <SafeAreaView style={styles.container}>
+      <StatusBar style="light" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen />
+        </Stack.Navigator>
+        <CategoriesScreen />
+      </NavigationContainer>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: "#24180f",
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: "center",
   },
 });
