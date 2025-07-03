0f31b98d6ee5284cbcbea016c60095cd34f767ef
Robert <Roberttovarv@gmail.com>
2025-07-03 17:49:41 +0200

    Still working...

--- components/MealItem.js
+++ components/MealItem.js
@@ -1,11 +1,31 @@
-import { View, Text } from "react-native"
+import { View, Text, Pressable, Image, StyleSheet } from "react-native";
 
-export const MealItem = ({title}) => {
-    return (
+export const MealItem = ({ title, imageUrl, complexity, affordability, duration }) => {
+  return (
+    <View>
+      <Pressable>
         <View>
-            <Text>
-        {title}
-            </Text>
+          <Text style={styles.title}>{title}</Text>
+          <Image source={{ uri: imageUrl }} style={styles.image} />
         </View>
-    )
-}
+        <View>
+            <Text>{duration} mins</Text>
+            <Text>{complexity.toUpperCase()}</Text>
+            <Text>{affordability.toUpperCase()}</Text>
+        </View>
+      </Pressable>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  image: {
+    width: "100%",
+    height: 200,
+  },
+  title: {
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 18,
+  },
+});
--- screens/MealsOverViewScreen.js
+++ screens/MealsOverViewScreen.js
@@ -3,22 +3,32 @@ import { MEALS } from "../data/dummy-data";
 import { MealItem } from "../components/MealItem";
 
 export const MealsOverviewScreen = ({ route }) => {
-    const categoryId = route.params.categoryId
-    const categoryTitle = route.params.categoryTitle
+  const categoryId = route.params.categoryId;
+  const categoryTitle = route.params.categoryTitle;
 
-    const displayedMeals = MEALS.filter((meal) => {
-        return meal.categoryIds.indexOf(categoryId) >= 0;
-    })
+  const displayedMeals = MEALS.filter((meal) => {
+    return meal.categoryIds.indexOf(categoryId) >= 0;
+  });
 
-    function renderMealItem(itemData) {
-        return (
-          <MealItem title = {categoryTitle} />
-        )
-    }
+  function renderMealItem(itemData) {
+    item = itemData.item;
+    const mealItemProps = {
+      title: item.title,
+      imageUrl: itemData.item.imageUrl,
+      duration: itemData.item.duration,
+      complexity: itemData.item.complexity,
+      affordability: itemData.item.affordability,
+    };
+    return <MealItem {...mealItemProps}/>;
+  }
 
   return (
     <View style={styles.container}>
-        <FlatList data ={displayedMeals} keyExtractor={(item) => item.id} renderItem={renderMealItem} />
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 };
